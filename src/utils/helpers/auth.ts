38d2020905355012353e0d4f69b0dc7b89b513ff
Lawal Hakeem <lawizyhal@gmail.com>
2022-02-16 13:18:35 +0100

    fix: bcrypt fix

--- src/utils/helpers/auth.ts
+++ src/utils/helpers/auth.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { jwtExpiresIn, jwtSecret, bcryptSalt } from "../../config";
 import User from "../../model/userModel";
