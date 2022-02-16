503d8275102ea3684472ef37bf9e35bd2eb5f394
Lawal Hakeem <lawizyhal@gmail.com>
2022-02-16 05:39:58 +0100

    fix: heroku db connect

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -2,10 +2,14 @@ import { Sequelize } from "sequelize";
 import { dbName, dbPassword, dbUsername } from "../config";
 import logger from "../utils/logger";
 
-export const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
-  host: "localhost",
-  dialect: "postgres"
-});
+// export const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
+//   host: "localhost",
+//   dialect: "postgres"
+// });
+
+export const sequelize = new Sequelize(
+  `postgres://${dbUsername}:${dbPassword}@127.0.0.1:5432/${dbName}`
+);
 
 export const connectToDB = async (): Promise<void> => {
   try {
